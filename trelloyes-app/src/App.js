249e782c4f9a35f610725cf438ef4e5800fbb82f
Laura Spieker <ljspiek@gmail.com>
2019-12-16 19:32:32 -0600

    begin refactoring code for add and delete

--- trelloyes-app/src/App.js
+++ trelloyes-app/src/App.js
@@ -2,29 +2,42 @@ import React from 'react';
 import './App.css';
 import List from './List';
 
-function App(props) {
- console.log(props);
-  return (
-    <main className='App'>
-      <header className='App-header'>
-        <h1>Trelloyes!</h1>
-      </header>
-      <div className='App-list'>
-     
-      {props.store.lists.map(list => (
-        <List 
-        key = {list.id}
-        header = {list.header}
-        cards = {list.cardIds.map(id => props.store.allCards[id])}
-        />
+class App extends React.Component(props) {
+  state = {
 
-        
-      ))}
-      </div>
-     
-   
-    </main>
-  );
+  }
+ 
+  handleDeleteItem(){
+    console.log('deleting item')
+  }
+
+  handleAddRandomCard() {
+    console.log('adding random card')
+  }
+ 
+  
+  render() {
+      return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+      
+        {props.store.lists.map(list => (
+          <List 
+          key = {list.id}
+          header = {list.header}
+          cards = {list.cardIds.map(id => props.store.allCards[id])}
+          />
+
+          
+        ))}
+        </div>
+      
+    
+      </main>
+    );
+  }
 }
 
-export default App;
